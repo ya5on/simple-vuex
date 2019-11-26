c5f774b9c66a0b6ac52d57eb3ca7c42e5492ecb8
ya5on <spaunfrom@gmail.com>
2019-11-26 22:28:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <PostFrom></PostFrom>
+    <h1>{{postsCount}}</h1>
+    <div class="post" v-for="post in validPosts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+import PostFrom from './components/PostFrom'
+
+export default {
+  name: 'app',
+  components: {PostFrom},
+  computed: mapGetters(["validPosts", 'postsCount']),
+  methods: mapActions(['fetchPosts']),
+  async mounted() {
+    // this.$store.dispatch('fetchPosts')
+    this.fetchPosts(4);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  width: 400px;
+}
+  .post {
+   border: 1px solid red;
+    margin-bottom: 1rem;
+  }
+</style>
---
+++ src/components/PostFrom.vue
@@ -0,0 +1,41 @@
+<template>
+    <form @submit.prevent="submit">
+        <input type="text" placeholder="title" v-model="title">
+        <input type="text" placeholder="body" v-model="body">
+        <button type="submit">Create post</button>
+        <hr>
+    </form>
+</template>
+
+<script>
+    import {mapMutations} from 'vuex'
+    export default {
+        data(){
+            return {
+                title: '',
+                body: ''
+            }
+        },
+        methods: {
+            ...mapMutations(['createPost']),
+            submit(){
+                this.createPost({
+                    title: this.title,
+                    body: this.body,
+                    id: Date.now()
+                });
+                this.title = this.body = ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    input {
+        display: block;
+        width: 100%;
+        border: 1px solid red;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+</style>
